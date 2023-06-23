92049f31713967a03a29ef1b877ca286e7b4dcb3
tanzim077 <tanzimahmed1994@gmail.com>
2023-06-23 14:26:57 +0600

    Socket function separate to another file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import {Box} from "@mui/material";
+import { Box } from "@mui/material";
 import Cookies from "js-cookie";
 import jwt_decode from "jwt-decode";
-import {useEffect} from "react";
-import {useDispatch, useSelector} from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import io from "socket.io-client";
 import "./App.css";
 import Routers from "./components/primary/Routers/Routers";
 import RoutersLogin from "./components/primary/Routers/RoutersLogin";
 import Layout from "./components/shared/Layout/Layout";
-import {availableJobsForReviewer, getAllAssignedJob, getAllJobs, getMyJobs} from "./features/slice/jobSlice";
 import {
-    deleteBefore15DaysNotifications,
-    getAllNotifications,
-    getAllUnreadNotifications,
-    getLatestNotifications,
-    setNewNotification,
+  availableJobsForReviewer,
+  getAllAssignedJob,
+  getAllJobs,
+  getMyJobs,
+} from "./features/slice/jobSlice";
+import {
+  deleteBefore15DaysNotifications,
+  getAllNotifications,
+  getAllUnreadNotifications,
+  getLatestNotifications,
+  setNewNotification,
 } from "./features/slice/notificationSlice";
-import {setFromPreviousTheme} from "./features/slice/themeSlice";
-import {alreadyLogin, updateLoggedInUserManually, updateSingleUserManually,} from "./features/slice/userSlice";
+import { setFromPreviousTheme } from "./features/slice/themeSlice";
+import {
+  alreadyLogin,
+  updateLoggedInUserManually,
+  updateSingleUserManually,
+} from "./features/slice/userSlice";
+import socketHandlers from "./socketHandlers";
 
 const CryptoJS = require("crypto-js");
 
@@ -28,7 +38,7 @@ function App() {
   const dispatch = useDispatch();
   const { user: storedUser } = useSelector((state) => state);
   const { isLoggedIn } = storedUser;
-  const { _id, role } = storedUser.user;
+
   const tokenCheck = () => {
     const existedToken = Cookies.get("token");
     if (existedToken) {
@@ -48,173 +58,18 @@ function App() {
   }, []);
 
   useEffect(() => {
-    const handleNotification = (notification) => {
-      if (
-        notification.notificationFor.includes(storedUser.user.role) ||
-        notification.notificationForUserIds.includes(storedUser.user._id)
-      ) {
-        dispatch(setNewNotification(notification));
-      }
-    };
-
-    const handleUserNotification = (notification, user) => {
-      storedUser.user._id === user._id &&
-        dispatch(updateLoggedInUserManually(user));
-      if (
-        notification.notificationFor.includes(storedUser.user.role) ||
-        notification.notificationForUserIds.includes(storedUser.user._id)
-      ) {
-        dispatch(setNewNotification(notification));
-      }
-    };
-
-    const handleJobBlockNotification = (notification, user) => {
-      storedUser.user._id === user._id &&
-        dispatch(updateLoggedInUserManually(user));
-      if (
-        notification.notificationFor.includes(storedUser.user.role) ||
-        notification.notificationForUserIds.includes(storedUser.user._id)
-      ) {
-        dispatch(getAllJobs());
-        notification.notificationFor.includes(storedUser.user._id) &&
-          dispatch(setNewNotification(notification));
-      }
-    };
-
-    const handleNewJobNotification = (notification, user) => {
-      if (
-        notification.notificationFor.includes(storedUser.user.role) ||
-        notification.notificationForUserIds.includes(storedUser.user._id)
-      ) {
-        dispatch(getAllJobs());
-        dispatch(setNewNotification(notification));
-      }
-    };
-
-    const handleAntNotification = (notification, project) => {
-      if (
-        project.activeHub.includes(storedUser.user.hub) &&
-        notification.notificationFor.includes(storedUser.user.role)
-      ) {
-        dispatch(setNewNotification(notification));
-        dispatch(getAllJobs());
-      }
-    };
-
-    const handleRMNotification = (notification, user) => {
-      dispatch(updateSingleUserManually({ user }));
-      if (
-        notification.notificationFor.includes(storedUser.user.role) ||
-        notification.notificationForUserIds.includes(storedUser.user._id)
-      ) {
-        dispatch(setNewNotification(notification));
-      }
-    };
-    const handleReviewerNotification = (notification, job) => {
-      if (
-        notification.notificationFor.includes(storedUser.user.role) ||
-        notification.notificationForUserIds.includes(storedUser.user._id)
-      ) {
-        dispatch(setNewNotification(notification));
-        dispatch(getMyJobs());
-        dispatch(availableJobsForReviewer());
-      }
-    };
-
-    const handleAnnotatorNotification = (notification, job) => {
-      if (
-        notification.notificationFor.includes(storedUser.user.role) ||
-        notification.notificationForUserIds.includes(storedUser.user._id)
-      ) {
-        dispatch(setNewNotification(notification));
-        dispatch(getMyJobs());
-      }
-    };
-
-    const handleJobTakersNotification = (notification, { job, targetUser }) => {
-      const storedUserRole = storedUser.user.role;
-      const storedUserId = storedUser.user._id;
-
-      if (targetUser._id === storedUserId) {
-        dispatch(updateLoggedInUserManually(targetUser));
-      }
-      if (notification.notificationFor.includes(storedUserRole)) {
-        dispatch(getAllJobs());
-        dispatch(getAllAssignedJob());
-      }
-      if (storedUserRole === "admin") {
-        dispatch(updateSingleUserManually({ user : targetUser }));
-      }
-    };
-
-    const customUserData = { _id, role };
-    socket.emit("customUserData", customUserData);
-    socket.on("notification", handleNotification);
-    socket.on("approvedUser", handleUserNotification);
-    socket.on("rejectUser", handleUserNotification);
-    socket.on("uploadNDAOrDocuments", handleRMNotification);
-    socket.on("updateUserRole", handleUserNotification);
-    socket.on("addUserSkills", handleUserNotification);
-    socket.on("removeUserSkills", handleUserNotification);
-    socket.on("blockUser", handleUserNotification);
-    socket.on("unBlockUser", handleUserNotification);
-    socket.on("updateProjectPriority", handleAntNotification);
-    socket.on("updateProjectHub", handleAntNotification);
-    socket.on("updateProjectStatus", handleAntNotification);
-    socket.on("newJobAvailable", handleNewJobNotification);
-    
-    socket.on("reviewerAvailableJob", handleReviewerNotification);
-    
-    
-    socket.on("takeJobReviewer", handleReviewerNotification);
-    socket.on("takeJobAnnotator", handleJobTakersNotification);
-    socket.on("reviewOnJobAccept", handleAnnotatorNotification);
-    socket.on("reviewOnJobReject", handleAnnotatorNotification);
-    socket.on("reviewOnJobRejectFinal", handleAnnotatorNotification);
-    socket.on("submitJob", handleReviewerNotification);
-    socket.on("jobTimeOutAnnotator", handleAnnotatorNotification);
-    socket.on("jobTimeOutReviewer", handleReviewerNotification);
-    socket.on("enrollCourseStudent", handleNotification);
-    socket.on("courseCompleteUser", handleNotification);
-    socket.on("benchmarkCreate", handleNotification);
-    socket.on("benchMarkUpdate", handleNotification);
-    socket.on("jobBlockAnnotator", handleJobBlockNotification);
-    socket.on("jobUnblockAnnotator", handleJobBlockNotification);
-
-    return () => {
-      socket.off("notification", handleNotification);
-      socket.off("approvedUser", handleUserNotification);
-      socket.off("rejectUser", handleUserNotification);
-      socket.off("uploadNDAOrDocuments", handleRMNotification);
-      socket.off("updateUserRole", handleUserNotification);
-      socket.off("addUserSkills", handleUserNotification);
-      socket.off("removeUserSkills", handleUserNotification);
-
-      socket.off("blockUser", handleUserNotification);
-      socket.off("unBlockUser", handleUserNotification);
-
-      socket.off("updateProjectPriority", handleAntNotification);
-      socket.off("updateProjectHub", handleAntNotification);
-      socket.off("updateProjectStatus", handleAntNotification);
-
-      socket.off("newJobAvailable", handleNewJobNotification);
-      socket.off("reviewerAvailableJob", handleReviewerNotification);
-
-
-      socket.off("takeJobReviewer", handleReviewerNotification);
-      socket.off("reviewOnJobAccept", handleAnnotatorNotification);
-      socket.off("reviewOnJobReject", handleAnnotatorNotification);
-      socket.off("reviewOnJobRejectFinal", handleAnnotatorNotification);
-      socket.off("submitJob", handleReviewerNotification);
-      socket.off("jobTimeOutAnnotator", handleAnnotatorNotification);
-      socket.off("jobTimeOutReviewer", handleReviewerNotification);
-      socket.off("enrollCourseStudent", handleNotification);
-      socket.off("courseCompleteUser", handleNotification);
-      socket.off("benchmarkCreate", handleNotification);
-      socket.off("benchMarkUpdate", handleNotification);
-      socket.off("jobBlockAnnotator", handleJobBlockNotification);
-      socket.off("jobUnblockAnnotator", handleJobBlockNotification);
-    };
+    socketHandlers({
+      socket,
+      dispatch,
+      storedUser,
+      setNewNotification,
+      updateLoggedInUserManually,
+      updateSingleUserManually,
+      getAllJobs,
+      getMyJobs,
+      availableJobsForReviewer,
+      getAllAssignedJob,
+    });
   }, [dispatch, storedUser.user.role]);
 
   useEffect(() => {

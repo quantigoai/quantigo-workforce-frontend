96bfa75f015ed4e76b356ed213099f9b1304d447
Tasfiq97 <tasfiqulazad200@gmail.com>
2023-09-26 11:06:44 +0600

    new commit

--- src/components/primary/ProjectLIstNew2/Project2Details/DetailsItemSIngle.jsx
+++ src/components/primary/ProjectLIstNew2/Project2Details/DetailsItemSIngle.jsx
@@ -1,35 +1,46 @@
 import { Grid, Stack, Typography } from "@mui/material";
 import React from "react";
+import { useSelector } from "react-redux";
 
 const DetailsItemSIngle = ({ Item1Title, Item1, Item2Title, Item2, Item3Title, Item3 }) => {
+  const { isLightTheme } = useSelector((user) => user.theme);
   return (
     <>
       <Stack sx={{ borderBottom: "1px solid #E6ECF5" }}>
         <Grid container>
           <Grid xs={4} sx={{ borderRight: "1px solid #E6ECF5", padding: "20px" }}>
-            <Typography variant="wf_h6_light" sx={{ color: "#091E42", opacity: "0.7" }}>
+            <Typography
+              variant="wf_h6_light"
+              sx={{ color: isLightTheme ? "#091E42" : "white", opacity: isLightTheme ? "0.7" : "1" }}
+            >
               {Item1Title}
             </Typography>
             <br />
-            <Typography variant="wf_h5_bold" sx={{ color: "#091E42", fontWeight: "500" }}>
+            <Typography variant="wf_h5_bold" sx={{ color: isLightTheme ? "#091E42" : "white", fontWeight: "500" }}>
               {Item1}
             </Typography>
           </Grid>
           <Grid xs={4} sx={{ borderRight: "1px solid #E6ECF5", padding: "20px" }}>
-            <Typography variant="wf_h6_light" sx={{ color: "#091E42", opacity: "0.7" }}>
+            <Typography
+              variant="wf_h6_light"
+              sx={{ color: isLightTheme ? "#091E42" : "white", opacity: isLightTheme ? "0.7" : "1" }}
+            >
               {Item2Title}
             </Typography>
             <br />
-            <Typography variant="wf_h5_bold" sx={{ color: "#091E42", fontWeight: "500" }}>
+            <Typography variant="wf_h5_bold" sx={{ color: isLightTheme ? "#091E42" : "white", fontWeight: "500" }}>
               {Item2}
             </Typography>
           </Grid>
           <Grid xs={4} sx={{ padding: "20px" }}>
-            <Typography variant="wf_h6_light" sx={{ color: "#091E42", opacity: "0.7" }}>
+            <Typography
+              variant="wf_h6_light"
+              sx={{ color: isLightTheme ? "#091E42" : "white", opacity: isLightTheme ? "0.7" : "1" }}
+            >
               {Item3Title}
             </Typography>
             <br />
-            <Typography variant="wf_h5_bold" sx={{ color: "#091E42", fontWeight: "500" }}>
+            <Typography variant="wf_h5_bold" sx={{ color: isLightTheme ? "#091E42" : "white", fontWeight: "500" }}>
               {Item3}
             </Typography>
           </Grid>

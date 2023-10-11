3b8bb9ebc797ae4fabe998ce56ceff6008e7a217
Tasfiq97 <tasfiqulazad200@gmail.com>
2023-10-11 15:00:11 +0600

    forgot pass updated

--- src/components/primary/HomePage/HeaderNav.jsx
+++ src/components/primary/HomePage/HeaderNav.jsx
@@ -14,7 +14,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 import logo from "../../../assets/images/logo.png";
 import logoBlue from "../../../assets/images/rsz_logoblue.png";
 
-const HeaderNav = ({ isForgetPassword }) => {
+const HeaderNav = ({ isForgetPassword, resetPassword }) => {
   const navigate = useNavigate();
   const location = useLocation();
   const { isLightTheme } = useSelector((state) => state.theme);
@@ -71,7 +71,7 @@ const HeaderNav = ({ isForgetPassword }) => {
                   display: "flex",
                   height: "100%",
                   justifyContent: "flex-end",
-                  backgroundColor: isForgetPassword ? "" : "#fff",
+                  backgroundColor: isForgetPassword || resetPassword ? "" : "#fff",
                   p: 4,
                 }}
                 gap={3}
@@ -84,7 +84,7 @@ const HeaderNav = ({ isForgetPassword }) => {
                     cursor: "pointer",
                   }}
                 >
-                  <Typography color={isForgetPassword ? "#fff" : "neutral.850"}>FAQ</Typography>
+                  <Typography color={isForgetPassword || resetPassword ? "#fff" : "neutral.850"}>FAQ</Typography>
                 </Link>
                 <Link
                   onClick={() => navigate("/howitworks")}
@@ -94,7 +94,7 @@ const HeaderNav = ({ isForgetPassword }) => {
                     cursor: "pointer",
                   }}
                 >
-                  <Typography color={isForgetPassword ? "#fff" : "neutral.850"} variant="body1">
+                  <Typography color={isForgetPassword || resetPassword ? "#fff" : "neutral.850"} variant="body1">
                     How it works
                   </Typography>
                 </Link>
@@ -102,7 +102,7 @@ const HeaderNav = ({ isForgetPassword }) => {
                   How it work
                 </Typography> */}
                 <Typography
-                  color={isForgetPassword ? "#fff" : "neutral.850"}
+                  color={isForgetPassword || resetPassword ? "#fff" : "neutral.850"}
                   // sx={{
                   //   color: location.pathname === "/howitworks" && " #47536B",
                   // }}

add229bf30bed1a1bb1cbb202d7eb06136255570
tanzim077 <tanzimahmed1994@gmail.com>
2023-10-13 14:21:51 +0600

    Header style fixed

--- src/components/shared/Header/Header.jsx
+++ src/components/shared/Header/Header.jsx
@@ -6,27 +6,28 @@
  *
  * Copyright (c) 2022 Tanzim Ahmed
  */
-import {Avatar, Badge, FormControlLabel, Stack, styled, Switch, Typography} from "@mui/material";
+import { Avatar, Badge, FormControlLabel, Stack, styled, Typography } from "@mui/material";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
-import React, {useState} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {useNavigate} from "react-router-dom";
-import backIcon from "../../../assets/images/dashboardIcon/GoBackIcon.svg";
+import Lottie from "lottie-react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import logOutIcon from "../../../assets/images/logoutIcon.svg";
 import menuIcon from "../../../assets/images/menuIcon.svg";
 import ProfileIcon from "../../../assets/images/profileIcon.svg";
 import useReset from "../../../customHooks/useReset";
-import {setTheme} from "../../../features/slice/themeSlice";
-import {logout} from "../../../features/slice/userSlice";
-import {capitalizeFirstLetter} from "../../../helper/capitalizeFirstWord";
-import NotificationModal from "../Notification/NotificationModal";
-import Lottie from "lottie-react";
+import { setTheme } from "../../../features/slice/themeSlice";
+import { logout } from "../../../features/slice/userSlice";
+import { capitalizeFirstLetter } from "../../../helper/capitalizeFirstWord";
 import bell from "../CustomSvgIcons/animation_lnnh2ad0.json";
+import NotificationModal from "../Notification/NotificationModal";
+import GoBackButton from "./GoBackButton";
+import ThemeSwitch from "./ThemeSwitch";
 
 const Header = () => {
   const { isLightTheme } = useSelector((state) => state.theme);
@@ -78,52 +79,6 @@ const Header = () => {
   const openPopover = Boolean(notificationOpen);
   const id = openPopover ? "simple-popover" : undefined;
 
-  const MaterialUISwitch = styled(Switch)(({ theme }) => ({
-    width: 60,
-    height: 35,
-    padding: 7,
-
-    "& .MuiSwitch-switchBase": {
-      padding: 0,
-      transform: "translateX(6px)",
-      "&.Mui-checked": {
-        color: "#fff",
-        transform: "translateX(22px)",
-        "& .MuiSwitch-thumb:before": {
-          backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-            "#fff"
-          )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
-        },
-        "& + .MuiSwitch-track": {
-          opacity: 1,
-          backgroundColor: theme.palette.mode === "dark" ? "#8796A5" : "#aab4be",
-        },
-      },
-    },
-    "& .MuiSwitch-thumb": {
-      backgroundColor: theme.palette.mode === "dark" ? "#003892" : "#001e3c",
-      width: 32,
-      height: 32,
-      "&:before": {
-        content: "''",
-        position: "absolute",
-        width: "100%",
-        height: "100%",
-        left: 0,
-        top: 0,
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center",
-        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-          "#fff"
-        )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
-      },
-    },
-    "& .MuiSwitch-track": {
-      opacity: 1,
-      backgroundColor: theme.palette.mode === "dark" ? "#8796A5" : "#aab4be",
-      borderRadius: 20 / 2,
-    },
-  }));
   const MyFormControlLabel = styled(FormControlLabel)(({ theme }) => ({
     "& .MuiFormControlLabel": {
       marginRight: "0px",
@@ -149,8 +104,7 @@ const Header = () => {
           sx={{
             mx: 0,
             py: 1,
-            px: 0,
-            // paddingLeft: "16px",
+            px: 2,
           }}
         >
           <Box
@@ -159,49 +113,20 @@ const Header = () => {
               justifyContent: "space-between",
             }}
           >
-            {/* Go Back button  */}
-            {/* TODO Move in a separate component */}
-            <Box
-              sx={{
-                display: "flex",
-              }}
-            >
-              <Button
-                sx={{
-                  color: "neutral.800",
-                  width: {
-                    xl: "100px",
-                    lg: "100px",
-                  },
-                  height: {
-                    xl: "100%",
-                    lg: "100%",
-                  },
-                  textTransform: "none",
-                }}
-                onClick={() => handleGoBack()}
-              >
-                <img src={backIcon} />
 
-                <Typography variant="wpf_p3_semiBold" sx={{ paddingLeft: "12%" }}>
-                  Go Back
-                </Typography>
-              </Button>
-            </Box>
+            <GoBackButton handleGoBack={handleGoBack} />
 
             <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
               <Box sx={{ display: "flex" }}>
                 <MyFormControlLabel
                   onClick={() => dispatch(setTheme(!isLightTheme))}
-                  control={<MaterialUISwitch sx={{ m: 0 }} checked={isLightTheme ? false : true} />}
+                  control={<ThemeSwitch sx={{ m: 0 }} checked={isLightTheme ? false : true} />}
                 />
 
                 <Box
                   sx={{
-                    // backgroundColor: "yellow",
                     display: "flex",
                     cursor: "pointer",
-                    // justifyContent: "flex-end",
                     alignItems: "center",
                     mr: 3,
                     transition: "all 0.3s ease-in-out",
@@ -215,15 +140,12 @@ const Header = () => {
                   onClick={handleNotificationOpen}
                 >
                   {allUnreadNotifications.length === 0 ? (
-                    // <NotificationsNoneIcon sx={{ color: "#1976d2" }} />
-                    // <NotificationsNoneIcon />
                     <>
                       <Lottie animationData={bell} {...lottieOptions} />
                     </>
                   ) : (
                     <>
                       <Badge sx={{}} badgeContent={allUnreadNotifications.length} color="primary">
-                        {/* <NotificationsActiveIcon /> */}
                         <Lottie animationData={bell} {...lottieOptions} />
                       </Badge>
                     </>

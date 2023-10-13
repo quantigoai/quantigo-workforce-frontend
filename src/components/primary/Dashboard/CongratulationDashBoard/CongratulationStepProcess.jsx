0fb298ddff5805061f0b19c05c8eb9b32ebd9ca7
Tasfiq97 <tasfiqulazad200@gmail.com>
2023-10-14 00:40:54 +0600

    process list updated

--- src/components/primary/Dashboard/CongratulationDashBoard/CongratulationStepProcess.jsx
+++ src/components/primary/Dashboard/CongratulationDashBoard/CongratulationStepProcess.jsx
@@ -1,58 +1,10 @@
 import { Box, Grid, Paper, Typography } from "@mui/material";
 import React from "react";
 import ProcessCard from "./ProcessCard";
+import processList from "./ProcessLIst";
+import ProcessCard2 from "./ProcessCard2";
 
 const CongratulationStepProcess = () => {
-  const projectList = [
-    {
-      header: "Create an Account",
-      describe: "Lorem ipsum dolor sit amet consectetur. Euismod mattis quam ut nulla eget",
-      active: true,
-      _id: 1,
-    },
-    {
-      header: "Verify your email address",
-      describe: "Lorem ipsum dolor sit amet consectetur. Euismod mattis quam ut nulla eget",
-      active: true,
-      _id: 2,
-    },
-    {
-      header: "Upload Identity  (NDA/ Documents)",
-      describe: "Lorem ipsum dolor sit amet consectetur. Euismod mattis quam ut nulla eget",
-      active: false,
-      _id: 3,
-    },
-    {
-      header: "Start a course",
-      describe: "Lorem ipsum dolor sit amet consectetur. Euismod mattis quam ut nulla eget",
-      active: false,
-      _id: 4,
-    },
-    {
-      header: "Achieve skills",
-      describe: "Lorem ipsum dolor sit amet consectetur. Euismod mattis quam ut nulla eget",
-      active: false,
-      _id: 5,
-    },
-    {
-      header: "Start Job",
-      describe: "Lorem ipsum dolor sit amet consectetur. Euismod mattis quam ut nulla eget",
-      active: false,
-      _id: 6,
-    },
-    {
-      header: "Submit Job",
-      describe: "Lorem ipsum dolor sit amet consectetur. Euismod mattis quam ut nulla eget",
-      active: false,
-      _id: 7,
-    },
-    {
-      header: "Earn Money",
-      describe: "Lorem ipsum dolor sit amet consectetur. Euismod mattis quam ut nulla eget",
-      active: false,
-      _id: 8,
-    },
-  ];
   const paperstyle = {
     backgroundColor: "neutral.N000",
     padding: ".5%",
@@ -69,7 +21,8 @@ const CongratulationStepProcess = () => {
           // height: "100%",
           borderRadius: "8px",
           // backgroundColor: "neutral.N000",
-        }}>
+        }}
+      >
         <Paper
           elevation={0}
           style={{
@@ -78,13 +31,14 @@ const CongratulationStepProcess = () => {
             // width: "100%",
             // height: "100%",
             borderRadius: "8px",
-          }}>
+          }}
+        >
           <Box sx={{ padding: "1%" }}>
             <Grid container>
-              <Typography variant="wpf_h4_semiBold" >Quantigo Workforce Process</Typography>
+              <Typography variant="wpf_h4_semiBold">Quantigo Workforce Process</Typography>
             </Grid>
             <Grid container>
-              <Typography variant="wpf_p3_regular" sx={{color:"neutral.N300"}}>
+              <Typography variant="wpf_p3_regular" sx={{ color: "neutral.N300" }}>
                 Lorem ipsum dolor sit amet consectetur. Libero faucibus dui faucibus et. Nisi amet viverra egestas vel
                 urna eu hendrerit. Mauris urna id dictum tortor. Vulputate vitae turpis duis sit. Id eleifend tincidunt
                 integer vitae scelerisque.
@@ -93,9 +47,10 @@ const CongratulationStepProcess = () => {
           </Box>
           <Box sx={{ padding: "1%" }}>
             <Grid container spacing={4}>
-              {projectList.map((item) => (
+              {processList.map((item) => (
                 <Grid key={item._id} item xs={12} sm={6} md={3} gap={1}>
-                  <ProcessCard item={item} />
+                  {/* <ProcessCard item={item} /> */}
+                  <ProcessCard2 item={item} />
                 </Grid>
               ))}
             </Grid>

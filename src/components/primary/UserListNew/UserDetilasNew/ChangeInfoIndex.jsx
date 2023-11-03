c7894670b07ef8c4b103bddc0278c779a27c7ecf
Tasfiq97 <tasfiqulazad200@gmail.com>
2023-11-04 01:50:11 +0600

    new commit

--- src/components/primary/UserListNew/UserDetilasNew/ChangeInfoIndex.jsx
+++ src/components/primary/UserListNew/UserDetilasNew/ChangeInfoIndex.jsx
@@ -6,45 +6,51 @@ import UserStatusChangeFiled from "./UserStatusChangeFiled";
 const ChangeInfoIndex = ({ user, handleSetRole, handleSetStatus, role }) => {
   return (
     <>
-      <Box sx={{ paddingTop: "2%", paddingBottom: "1%" }}>
-        <Typography
-          variant="wpf_p3_semiBold"
-          // sx={{ fontSize: "14px" }}
-        >
-          Change Info.
-        </Typography>
-      </Box>
-      <Box>
-        <Stack
-          sx={{
-            border: "1px solid #E6ECF5",
-            padding: "2%",
-            borderRadius: "8px",
-            // backgroundColor: "#F2F6FC",
-            backgroundColor: "neutral.N400",
-          }}
-        >
-          <Grid container>
-            <Grid
-              item
-              xs={role === "recruitment_manager" ? 12 : 6}
-              sx={{ paddingRight: role === "recruitment_manager" ? "" : "2%" }}
+      {role === "account_manager" ? (
+        <></>
+      ) : (
+        <Box>
+          <Box sx={{ paddingTop: "2%", paddingBottom: "1%" }}>
+            <Typography
+              variant="wpf_p3_semiBold"
+              // sx={{ fontSize: "14px" }}
             >
-              <SelectFieldCommon label={"Role Change"} user={user} handleSetRole={handleSetRole} />
-            </Grid>
-            {role === "recruitment_manager" ? (
-              <></>
-            ) : (
-              <Grid item xs={6}>
-                <UserStatusChangeFiled label={"Status"} handleSetStatus={handleSetStatus} />
+              Change Info.
+            </Typography>
+          </Box>
+          <Box>
+            <Stack
+              sx={{
+                border: "1px solid #E6ECF5",
+                padding: "2%",
+                borderRadius: "8px",
+                // backgroundColor: "#F2F6FC",
+                backgroundColor: "neutral.N400",
+              }}
+            >
+              <Grid container>
+                <Grid
+                  item
+                  xs={role === "recruitment_manager" ? 12 : 6}
+                  sx={{ paddingRight: role === "recruitment_manager" ? "" : "2%" }}
+                >
+                  <SelectFieldCommon label={"Role Change"} user={user} handleSetRole={handleSetRole} />
+                </Grid>
+                {role === "recruitment_manager" ? (
+                  <></>
+                ) : (
+                  <Grid item xs={6}>
+                    <UserStatusChangeFiled label={"Status"} handleSetStatus={handleSetStatus} />
+                  </Grid>
+                )}
               </Grid>
-            )}
-          </Grid>
-          {/* <Grid container>
+              {/* <Grid container>
             <Grid item xs={12}></Grid>
           </Grid> */}
-        </Stack>
-      </Box>
+            </Stack>
+          </Box>
+        </Box>
+      )}
     </>
   );
 };

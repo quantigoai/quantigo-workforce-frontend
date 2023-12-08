e44234236e3f95c013e5cbb77929e922c1a3cb2f
Tasfiq97 <tasfiqulazad200@gmail.com>
2023-12-08 14:58:05 +0600

    new commit

--- src/components/primary/Course/SingleChapterNew.jsx
+++ src/components/primary/Course/SingleChapterNew.jsx
@@ -88,7 +88,7 @@ const SingleChapterNew = ({ courseChapter, index, role }) => {
                     borderRadius: "20px",
                     width: "25px",
                     height: "25px",
-                    // marginRight: "10px",
+                    marginRight: "10px",
                   }}
                 >
                   {index + 1}

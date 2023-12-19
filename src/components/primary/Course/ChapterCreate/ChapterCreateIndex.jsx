c095eae7f45713b72c2ee336c71f6ab231051960
Tasfiq97 <tasfiqulazad200@gmail.com>
2023-12-19 11:08:51 +0600

    chapter create update hook

--- src/components/primary/Course/ChapterCreate/ChapterCreateIndex.jsx
+++ src/components/primary/Course/ChapterCreate/ChapterCreateIndex.jsx
@@ -1,33 +1,30 @@
-import { yupResolver } from "@hookform/resolvers/yup";
 import { Box, Grid } from "@mui/material";
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate, useParams } from "react-router-dom";
-import * as Yup from "yup";
-import useToaster from "../../../../customHooks/useToaster";
-import { createCourseChapter } from "../../../../features/slice/courseSlice";
-import { realToken } from "../../../../helper/lib";
 import FormProvider from "../../../shared/FormProvider/FormProvider";
 import ContentField from "../InputFields/ContentField";
 import ChapterCreateHeader from "./ChapterCreateHeader";
 import ChapterDIsableNoFIeld from "./ChapterDIsableNoFIeld";
 import ChapterDescritionField from "./ChapterDescritionField";
 import ChapterField from "./ChapterField";
+import useChapterManage from "../hooks/CourseChapter/useChapterManage";
+import { useEffect } from "react";
+
 const ChapterCreateIndex = () => {
-  const { courseChapters } = useSelector((state) => state.course);
-  const { course, isLoading } = useSelector((state) => state.course);
-  const navigate = useNavigate();
-  const params = useParams();
-  const toast = useToaster();
-  const id = params.id;
-  const UPLOAD_ENDPOINT = "courses/couseimages/uploads";
-  const API_URl = import.meta.env.VITE_APP_SERVER_URL;
-  const [content, setContent] = useState("");
-  const dispatch = useDispatch();
-  const [durationTime, setDurationTime] = useState("");
-  const [isDisable, setIsDisable] = useState(true);
+  const {
+    methods,
+    handleSubmit,
+    onSubmit,
+    isDisable,
+    isLoading,
+    durationTime,
+    courseChapters,
+    uploadPlugin,
+    setContent,
+    isFieldNotEmpty,
+    isInValid,
+    setDurationTime,
+    setIsDisable,
+  } = useChapterManage();
+
   useEffect(() => {
     const duration = courseChapters?.reduce((accumulator, currentValue) => {
       return accumulator + currentValue.estimatedTimeToRead || 0;
@@ -44,62 +41,7 @@ const ChapterCreateIndex = () => {
       setDurationTime(hours + " hours " + minutes + " minutes");
     }
   }, []);
-  function uploadAdapter(loader) {
-    return {
-      upload: () => {
-        return new Promise((resolve, reject) => {
-          const body = new FormData();
-          loader.file.then((file) => {
-            body.append("uploadImg", file);
-            body.append("courseId", id);
-            axios
-              .post(`${API_URl}/${UPLOAD_ENDPOINT}`, body, {
-                headers: {
-                  Authorization: `Bearer ${realToken()}`,
-                },
-              })
-              .then((res) => {
-                return res.data;
-              })
-              .then((res) => {
-                resolve({ default: res });
-                // dispatch(updateTemporaryData({ id, chapterNo, links: res }));
-              })
-              .catch((err) => {
-                reject(err);
-              });
-          });
-        });
-      },
-    };
-  }
 
-  function uploadPlugin(editor) {
-    editor.plugins.get("FileRepository").createUploadAdapter = (loader) => {
-      return uploadAdapter(loader);
-    };
-  }
-  const CourseCreateSchema = Yup.object().shape({
-    title: Yup.string().required("Course title is required"),
-    estimatedTimeToRead: Yup.number()
-      .required("Chapter estimated time is required")
-      .lessThan(21, "Chapter estimated time must be in range between 1 to 20")
-      .transform((value) => (isNaN(value) ? undefined : value)),
-    description: Yup.string().required("Chapter description is required"),
-  });
-  const methods = useForm({
-    resolver: yupResolver(CourseCreateSchema),
-    mode: "all",
-  });
-
-  const {
-    watch,
-    handleSubmit,
-    formState: { errors },
-  } = methods;
-  const { title, estimatedTimeToRead, description } = watch();
-  const isFieldNotEmpty = !!title && !!estimatedTimeToRead && !!description;
-  const isInValid = errors.estimatedTimeToRead;
   useEffect(() => {
     if (isFieldNotEmpty && !isInValid?.message) {
       setIsDisable(false);
@@ -107,23 +49,7 @@ const ChapterCreateIndex = () => {
       setIsDisable(true);
     }
   }, [isFieldNotEmpty, isInValid?.message]);
-  const onSubmit = (data) => {
-    const finalData = {
-      ...data,
-      // chapterNo,
-      rootCourse: id,
-      content,
-    };
-    dispatch(createCourseChapter(finalData)).then((action) => {
-      if (action.payload?.status === 201) {
-        toast.trigger(action.payload.data.message, "success");
-        // dispatch(deleteTemporaryData({ id, chapterNo }));
-        navigate(`/quiz-create/${id}`);
-      } else {
-        toast.trigger(action.error.message, "error");
-      }
-    });
-  };
+
   return (
     <Box className="content" sx={{ backgroundColor: "neutral.N000" }}>
       <Grid container sx={{ borderTop: "1px solid #E6ECF5", paddingTop: "1%" }}>
---
+++ src/components/primary/Course/hooks/CourseChapter/useChapterManage.jsx
@@ -0,0 +1,114 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import useToaster from "../../../../../customHooks/useToaster";
+import { useState } from "react";
+import * as Yup from "yup";
+import { createCourseChapter } from "../../../../../features/slice/courseSlice";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { realToken } from "../../../../../helper/lib";
+const useChapterManage = () => {
+  const { courseChapters } = useSelector((state) => state.course);
+  const { course, isLoading } = useSelector((state) => state.course);
+  const navigate = useNavigate();
+  const params = useParams();
+  const toast = useToaster();
+  const id = params.id;
+  const UPLOAD_ENDPOINT = "courses/couseimages/uploads";
+  const API_URl = import.meta.env.VITE_APP_SERVER_URL;
+  const [content, setContent] = useState("");
+  const dispatch = useDispatch();
+  const [durationTime, setDurationTime] = useState("");
+  const [isDisable, setIsDisable] = useState(true);
+  function uploadAdapter(loader) {
+    return {
+      upload: () => {
+        return new Promise((resolve, reject) => {
+          const body = new FormData();
+          loader.file.then((file) => {
+            body.append("uploadImg", file);
+            body.append("courseId", id);
+            axios
+              .post(`${API_URl}/${UPLOAD_ENDPOINT}`, body, {
+                headers: {
+                  Authorization: `Bearer ${realToken()}`,
+                },
+              })
+              .then((res) => {
+                return res.data;
+              })
+              .then((res) => {
+                resolve({ default: res });
+                // dispatch(updateTemporaryData({ id, chapterNo, links: res }));
+              })
+              .catch((err) => {
+                reject(err);
+              });
+          });
+        });
+      },
+    };
+  }
+
+  function uploadPlugin(editor) {
+    editor.plugins.get("FileRepository").createUploadAdapter = (loader) => {
+      return uploadAdapter(loader);
+    };
+  }
+  const CourseCreateSchema = Yup.object().shape({
+    title: Yup.string().required("Course title is required"),
+    estimatedTimeToRead: Yup.number()
+      .required("Chapter estimated time is required")
+      .lessThan(21, "Chapter estimated time must be in range between 1 to 20")
+      .transform((value) => (isNaN(value) ? undefined : value)),
+    description: Yup.string().required("Chapter description is required"),
+  });
+  const methods = useForm({
+    resolver: yupResolver(CourseCreateSchema),
+    mode: "all",
+  });
+
+  const {
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = methods;
+  const { title, estimatedTimeToRead, description } = watch();
+  const isFieldNotEmpty = !!title && !!estimatedTimeToRead && !!description;
+  const isInValid = errors.estimatedTimeToRead;
+
+  const onSubmit = (data) => {
+    const finalData = {
+      ...data,
+      // chapterNo,
+      rootCourse: id,
+      content,
+    };
+    dispatch(createCourseChapter(finalData)).then((action) => {
+      if (action.payload?.status === 201) {
+        toast.trigger(action.payload.data.message, "success");
+        // dispatch(deleteTemporaryData({ id, chapterNo }));
+        navigate(`/quiz-create/${id}`);
+      } else {
+        toast.trigger(action.error.message, "error");
+      }
+    });
+  };
+  return {
+    methods,
+    handleSubmit,
+    onSubmit,
+    isDisable,
+    isLoading,
+    durationTime,
+    courseChapters,
+    uploadPlugin,
+    setContent,
+    isFieldNotEmpty,
+    isInValid,
+    setIsDisable,
+    setDurationTime,
+  };
+};
+
+export default useChapterManage;

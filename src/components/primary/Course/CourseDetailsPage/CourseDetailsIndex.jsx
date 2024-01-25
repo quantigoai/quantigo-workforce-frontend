47642332ba4f1056a219a08672377de27812fa01
tanzim077 <tanzimahmed1994@gmail.com>
2024-01-25 11:08:06 +0600

    Refactor course details components

--- src/components/primary/Course/CourseDetailsPage/CourseDetailsIndex.jsx
+++ src/components/primary/Course/CourseDetailsPage/CourseDetailsIndex.jsx
@@ -1,16 +1,16 @@
-import React, {useEffect} from "react";
-import {useDispatch} from "react-redux";
-import {useParams, useSearchParams} from "react-router-dom";
-import {setActiveChapterIndex} from "../../../../features/slice/activePathSlice";
-import {getAllChapterFromACourse} from "../../../../features/slice/courseSlice";
-import CourseContentIndex from "./CourseContentIndex";
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useParams, useSearchParams } from 'react-router-dom';
+import { setActiveChapterIndex } from '../../../../features/slice/activePathSlice';
+import { getAllChapterFromACourse } from '../../../../features/slice/courseSlice';
+import CourseContentIndex from './CourseContentIndex';
 
 const CourseDetailsIndex = () => {
   let [redirectParams, setSearchParams] = useSearchParams();
   const params = useParams();
   const id = params.id;
 
-  const isRedirect = redirectParams.get("isRedirect");
+  const isRedirect = redirectParams.get('isRedirect');
   const dispatch = useDispatch();
 
   useEffect(() => {

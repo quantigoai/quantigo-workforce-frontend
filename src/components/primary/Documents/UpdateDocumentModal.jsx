97df3479099ef99ae29f5b2117ece616f483059c
tanzim077 <tanzimahmed1994@gmail.com>
2024-01-26 22:30:59 +0600

    Refactor ModalImage component

--- src/components/primary/Documents/UpdateDocumentModal.jsx
+++ src/components/primary/Documents/UpdateDocumentModal.jsx
@@ -1,48 +1,58 @@
-import { LoadingButton } from "@mui/lab";
-import { Alert, Box, Button, Grid, MenuItem, Modal, Select, TextField, Typography } from "@mui/material";
-import { styled } from "@mui/material/styles";
-import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import useToaster from "../../../customHooks/useToaster";
-import { updateMyDocuments } from "../../../features/slice/userSlice";
-import ProjectModalHeader from "../ProjectLIstNew2/ProjectModalHeader";
-import UploadImagesField from "../EditProfile/EditProfilePage/VerificationInfo/UploadImagesField";
-import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import { LoadingButton } from '@mui/lab';
+import {
+  Alert,
+  Box,
+  Button,
+  Grid,
+  MenuItem,
+  Modal,
+  Select,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { styled } from '@mui/material/styles';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import useToaster from '../../../customHooks/useToaster';
+import { updateMyDocuments } from '../../../features/slice/userSlice';
+import UploadImagesField from '../EditProfile/EditProfilePage/VerificationInfo/UploadImagesField';
+import ProjectModalHeader from '../ProjectLIstNew2/ProjectModalHeader';
 export const MyTextField = styled(TextField)(() => ({
-  "& .MuiOutlinedInput-notchedOutline": {
-    border: "2px solid #E6ECF5 !important",
-    borderRadius: "8px",
+  '& .MuiOutlinedInput-notchedOutline': {
+    border: '2px solid #E6ECF5 !important',
+    borderRadius: '8px',
   },
-  "& .MuiInputBase-root": {
-    height: "80%",
-    fontSize: "14px",
-    color: "neutral.N000",
+  '& .MuiInputBase-root': {
+    height: '80%',
+    fontSize: '14px',
+    color: 'neutral.N000',
   },
 }));
 export const MySelect = styled(Select)(() => ({
-  border: "2px solid #E6ECF5",
-  borderRadius: "8px",
-  background: "none",
-  fontSize: "14px",
-  height: "7%",
+  border: '2px solid #E6ECF5',
+  borderRadius: '8px',
+  background: 'none',
+  fontSize: '14px',
+  height: '7%',
 }));
 const style = {
-  display: "flex",
-  flexDirection: "column",
-  position: "relative",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  bgcolor: "background.paper",
-  border: "none",
-  borderRadius: "8px",
+  display: 'flex',
+  flexDirection: 'column',
+  position: 'relative',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  bgcolor: 'background.paper',
+  border: 'none',
+  borderRadius: '8px',
   p: 0,
   input: {
-    height: "20px",
-    borderRadius: "8px",
+    height: '20px',
+    borderRadius: '8px',
   },
   select: {
-    height: "20px",
+    height: '20px',
   },
 };
 
@@ -53,13 +63,17 @@ const UpdateDocumentModal = ({ openModal, setOpenModalNid }) => {
   const dispatch = useDispatch();
   const [documentsType, setDocumentsType] = useState(user?.documentsType);
   const [documentNo, setDocumentNo] = useState(user?.documentNo);
-  const [imagesCopy, setImagesCopy] = useState(user?.documentsImage);
+  const [imagesCopy, setImagesCopy] = useState(
+    user?.documentsImage?.length ? user.documentsImage : [],
+  );
   const [isDocumentNoValid, setDocumentNoValid] = useState(false);
   const [isDocumentTypeValid, setDocumentTypeValid] = useState(false);
-  const [images, setImages] = useState(user?.documentsImage);
+  const [images, setImages] = useState(
+    user?.documentsImage?.length ? user.documentsImage : [],
+  );
   const [removeImagesUpdate, setRemoveImagesUpdate] = useState([
     {
-      name: "",
+      name: '',
       isRemoved: false,
     },
   ]);
@@ -71,7 +85,7 @@ const UpdateDocumentModal = ({ openModal, setOpenModalNid }) => {
     const documentNo = e.target.value;
     setDocumentNo(e.target.value);
     // Check if the document number is not empty
-    setDocumentNoValid(documentNo.trim() !== "");
+    setDocumentNoValid(documentNo.trim() !== '');
   };
 
   const handleDocumentTypeChange = (e) => {
@@ -79,7 +93,7 @@ const UpdateDocumentModal = ({ openModal, setOpenModalNid }) => {
     setDocumentsType(e.target.value);
     // Check if a document type is selected
 
-    setDocumentTypeValid(documentType !== "");
+    setDocumentTypeValid(documentType !== '');
   };
   const handleImage = (e) => {
     setCoverImageFile(e[0]);
@@ -100,31 +114,34 @@ const UpdateDocumentModal = ({ openModal, setOpenModalNid }) => {
     setDocumentsType(user?.documentsType);
     setDocumentNo(user?.documentNo);
     setImages(user?.documentsImage);
-    setImagesCopy(user?.documentNo);
+    setImagesCopy(user.documentNo ? user.documentNo : []);
   };
   // const onSubmit = (data) => {
   const handleChange = (data) => {
     const formData = new FormData();
     // formData.append("documentsImage", coverImageFile);
-    formData.append("documentsType", documentsType);
-    formData.append("documentNo", documentNo);
+    formData.append('documentsType', documentsType);
+    formData.append('documentNo', documentNo);
     images.forEach((item) => {
       if (item.name) {
-        formData.append("documentsImage", item);
+        formData.append('documentsImage', item);
       }
     });
 
     if (imagesCopy.length != 0) {
       imagesCopy.map((item, index) => {
         const tempData = {
-          name: "",
+          name: '',
           isRemoved: false,
         };
         const isRemoved = removeImages.includes(item);
         tempData.name = item;
         tempData.isRemoved = isRemoved;
         formData.append(`removedImages[${index}][name]`, tempData.name);
-        formData.append(`removedImages[${index}][isRemoved]`, tempData.isRemoved);
+        formData.append(
+          `removedImages[${index}][isRemoved]`,
+          tempData.isRemoved,
+        );
       });
     }
     const finalData = {
@@ -134,9 +151,12 @@ const UpdateDocumentModal = ({ openModal, setOpenModalNid }) => {
 
     dispatch(updateMyDocuments(finalData)).then((action) => {
       if (action.error) {
-        toast.trigger(action.error.message, "error");
+        toast.trigger(action.error.message, 'error');
       } else {
-        toast.trigger("Your Documents has been update successfully.", "success");
+        toast.trigger(
+          'Your Documents has been update successfully.',
+          'success',
+        );
         handleClose();
         setCoverImage(null);
         setDocumentNoValid(false);
@@ -157,19 +177,22 @@ const UpdateDocumentModal = ({ openModal, setOpenModalNid }) => {
           sx={{
             ...style,
             height: {
-              lg: "80%",
-              xl: "74%",
-              xxl: "70%",
+              lg: '80%',
+              xl: '74%',
+              xxl: '70%',
             },
             width: {
-              lg: "50%",
-              xl: "45%",
-              xxl: "45%",
+              lg: '50%',
+              xl: '45%',
+              xxl: '45%',
             },
           }}
         >
-          <Box sx={{ flex: "0 0 5%" }}>
-            <ProjectModalHeader handleCreateProjectClose={handleClose} modalTitle={"Upload Document"} />
+          <Box sx={{ flex: '0 0 5%' }}>
+            <ProjectModalHeader
+              handleCreateProjectClose={handleClose}
+              modalTitle={'Upload Document'}
+            />
           </Box>
           {user?.rejectionCause && (
             <Box sx={{}}>
@@ -177,10 +200,10 @@ const UpdateDocumentModal = ({ openModal, setOpenModalNid }) => {
                 variant="filled"
                 severity="warning"
                 sx={{
-                  border: "1px solid #F2A200",
-                  color: "warning.400",
-                  backgroundColor: "warning.100",
-                  borderRadius: "6px",
+                  border: '1px solid #F2A200',
+                  color: 'warning.400',
+                  backgroundColor: 'warning.100',
+                  borderRadius: '6px',
                   // height: "48px",
                   // fontSize: "12px",
                 }}
@@ -191,23 +214,23 @@ const UpdateDocumentModal = ({ openModal, setOpenModalNid }) => {
           )}
           <Box
             sx={{
-              flex: "1",
-              overflowY: "scroll",
-              padding: "3%",
-              "&::-webkit-scrollbar": {
-                width: "0",
+              flex: '1',
+              overflowY: 'scroll',
+              padding: '3%',
+              '&::-webkit-scrollbar': {
+                width: '0',
               },
             }}
           >
             <Box>
-              <Grid container sx={{ padding: "0%", paddingLeft: "0%" }}>
+              <Grid container sx={{ padding: '0%', paddingLeft: '0%' }}>
                 <Typography
-                  variant={"wpf_p4_semiBold"}
+                  variant={'wpf_p4_semiBold'}
                   sx={{
                     mb: 1,
                   }}
                 >
-                  Document Type<span style={{ color: "red" }}>*</span>
+                  Document Type<span style={{ color: 'red' }}>*</span>
                 </Typography>
 
                 <Select
@@ -216,41 +239,44 @@ const UpdateDocumentModal = ({ openModal, setOpenModalNid }) => {
                   variant="outlined"
                   fullWidth
                   placeholder="Select"
-                  value={documentsType}
+                  value={documentsType ?? ''}
                   sx={{
-                    border: "2px solid #E6ECF5",
-                    borderRadius: "8px",
-                    background: "none",
-                    fontSize: "14px",
-                    height: "43px",
+                    border: '2px solid #E6ECF5',
+                    borderRadius: '8px',
+                    background: 'none',
+                    fontSize: '14px',
+                    height: '43px',
                   }}
                   onChange={(e) => {
                     // setDocumentsType(e.target.value);
                     handleDocumentTypeChange(e); // Validate document type
                   }}
                 >
-                  <MenuItem value={"NID"} sx={{ fontSize: "14px" }}>
+                  <MenuItem value={'NID'} sx={{ fontSize: '14px' }}>
                     NID
                   </MenuItem>
-                  <MenuItem value={"passport"} sx={{ fontSize: "14px" }}>
+                  <MenuItem value={'passport'} sx={{ fontSize: '14px' }}>
                     Passport
                   </MenuItem>
                 </Select>
               </Grid>
 
-              <Grid container sx={{ padding: "0%", paddingTop: "2%", paddingBottom: "1%" }}>
+              <Grid
+                container
+                sx={{ padding: '0%', paddingTop: '2%', paddingBottom: '1%' }}
+              >
                 <Typography
-                  variant={"wpf_p4_semiBold"}
+                  variant={'wpf_p4_semiBold'}
                   sx={{
                     mb: 1,
                   }}
                 >
-                  Document No.<span style={{ color: "red" }}>*</span>
+                  Document No.<span style={{ color: 'red' }}>*</span>
                 </Typography>
                 <MyTextField
                   variant="outlined"
                   fullWidth
-                  value={documentNo}
+                  value={documentNo ?? ''}
                   InputProps={{ disableUnderline: true }}
                   placeholder="Enter Document No."
                   onChange={(e) => {
@@ -261,16 +287,16 @@ const UpdateDocumentModal = ({ openModal, setOpenModalNid }) => {
                 />
               </Grid>
 
-              <Grid  sx={{}}>
+              <Grid sx={{}}>
                 <UploadImagesField
                   editAble={true}
-                  label={"Document "}
+                  label={'Document '}
                   files={images}
                   setFiles={setImages}
                   setImagesCopy={setImagesCopy}
                   imagesCopy={imagesCopy}
                   setRemoveImages={setRemoveImages}
-                  documentType={"documentType"}
+                  documentType={'documentType'}
                 />
                 {/* <DocumentImageUpload
                   coverImageFile={coverImageFile}
@@ -285,29 +311,29 @@ const UpdateDocumentModal = ({ openModal, setOpenModalNid }) => {
 
           <Box
             sx={{
-              flex: "0 0 64px",
-              borderTop: "2px solid #F2F6FC",
-              backgroundColor: "neutral.N000",
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-              padding: "0 2%",
-              bottom: "0px",
-              borderRadius: "8px",
+              flex: '0 0 64px',
+              borderTop: '2px solid #F2F6FC',
+              backgroundColor: 'neutral.N000',
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              padding: '0 2%',
+              bottom: '0px',
+              borderRadius: '8px',
             }}
           >
-            <Grid container sx={{ padding: "2%" }}>
+            <Grid container sx={{ padding: '2%' }}>
               <Grid item xs={6}>
                 <Button
                   sx={{
-                    width: "120px",
-                    textTransform: "none",
-                    backgroundColor: "primary.B008",
-                    color: "neutral.N650",
-                    borderRadius: "8px",
-                    "&:hover": {
-                      backgroundColor: "neutral.N600",
-                      color: "neutral.N650",
+                    width: '120px',
+                    textTransform: 'none',
+                    backgroundColor: 'primary.B008',
+                    color: 'neutral.N650',
+                    borderRadius: '8px',
+                    '&:hover': {
+                      backgroundColor: 'neutral.N600',
+                      color: 'neutral.N650',
                     },
                   }}
                   onClick={() => handleClose()}
@@ -317,7 +343,7 @@ const UpdateDocumentModal = ({ openModal, setOpenModalNid }) => {
               </Grid>
 
               <Grid item xs={6}>
-                <Grid container sx={{ justifyContent: "right" }}>
+                <Grid container sx={{ justifyContent: 'right' }}>
                   <LoadingButton
                     type="submit"
                     // disabled={true}
@@ -331,20 +357,22 @@ const UpdateDocumentModal = ({ openModal, setOpenModalNid }) => {
                     //   // documentNo && documentsType &&
                     //   images.length === 0
                     // }
-                    disabled={!documentsType || !documentNo || images.length === 0}
+                    disabled={
+                      !documentsType || !documentNo || images.length === 0
+                    }
                     sx={{
-                      width: "128px",
-                      textTransform: "none",
-                      backgroundColor: "#2E58FF",
-                      color: "#FFFFFF",
-                      borderRadius: "8px",
-                      "&:hover": {
-                        backgroundColor: "#476CFF",
-                        color: "#FFFFFF",
+                      width: '128px',
+                      textTransform: 'none',
+                      backgroundColor: '#2E58FF',
+                      color: '#FFFFFF',
+                      borderRadius: '8px',
+                      '&:hover': {
+                        backgroundColor: '#476CFF',
+                        color: '#FFFFFF',
                       },
-                      "&.Mui-disabled": {
-                        background: "#B6C9F0",
-                        color: "#FFFFFF",
+                      '&.Mui-disabled': {
+                        background: '#B6C9F0',
+                        color: '#FFFFFF',
                       },
                     }}
                     onClick={handleChange}

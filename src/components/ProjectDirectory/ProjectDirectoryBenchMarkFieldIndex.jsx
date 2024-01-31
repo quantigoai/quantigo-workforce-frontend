9f95905159c1f52a944798bc8821dde7a8509170
Tasfiq97 <tasfiqulazad200@gmail.com>
2024-01-31 14:56:20 +0600

    new

--- src/components/ProjectDirectory/ProjectDirectoryBenchMarkFieldIndex.jsx
+++ src/components/ProjectDirectory/ProjectDirectoryBenchMarkFieldIndex.jsx
@@ -1,30 +1,29 @@
-import { Box, Grid, Menu, MenuItem, Stack, Typography } from "@mui/material";
-import React, { useState } from "react";
-import { useSelector } from "react-redux";
-import { FieldBox } from "../shared/FIeldbox/FieldBox";
-import PDTextFIeld from "../shared/CustomField/PDTextFIeld";
-import TextFieldProjectDirectoryBenchmark from "../shared/CustomField/TextFieldProjectDirectoryBenchmark";
+import { Box, Grid, Menu, MenuItem, Stack, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+
+import TextFieldProjectDirectoryBenchmark from '../shared/CustomField/TextFieldProjectDirectoryBenchmark';
 const addBenchmarkType = [
-  { item: "Client Alias", value: "Client_Alias" },
-  { item: "Industry", value: "Industry" },
-  { item: "Platform", value: "Platform" },
-  { item: "Tool Type", value: "Tool_Type" },
-  { item: "PDR", value: "PDR" },
-  { item: "Project Type", value: "Project_Type" },
-  { item: "Action Item", value: "Action_Items" },
-  { item: "QA Check Points", value: "QA_Check_Points" },
-  { item: "Object Benchmark", value: "Obj_Benchmark" },
-  { item: "Image Benchmark", value: "Img_Benchmark" },
-  { item: "Tagging Benchmark", value: "Tagging_Benchmark" },
-  { item: "Deletion", value: "Deletion" },
-  { item: "Skip Image", value: "Skip_Image" },
-  { item: "Image Loading", value: "Image_Loading" },
-  { item: "Object Saving Time", value: "Object_Saving_Time" },
-  { item: "Video Watch Time", value: "Video_Watch_Time" },
-  { item: "Judgement Time", value: "Judgement_Time" },
-  { item: "QA Benchmark", value: "QA_Benchmark" },
-  { item: "Annotation", value: "Annotation" },
-  { item: "QA", value: "QA" },
+  { item: 'Client Alias', value: 'Client_Alias' },
+  { item: 'Industry', value: 'Industry' },
+  { item: 'Platform', value: 'Platform' },
+  { item: 'Tool Type', value: 'Tool_Type' },
+  { item: 'PDR', value: 'PDR' },
+  { item: 'Project Type', value: 'Project_Type' },
+  { item: 'Action Item', value: 'Action_Items' },
+  { item: 'QA Check Points', value: 'QA_Check_Points' },
+  { item: 'Object Benchmark', value: 'Obj_Benchmark' },
+  { item: 'Image Benchmark', value: 'Img_Benchmark' },
+  { item: 'Tagging Benchmark', value: 'Tagging_Benchmark' },
+  { item: 'Deletion', value: 'Deletion' },
+  { item: 'Skip Image', value: 'Skip_Image' },
+  { item: 'Image Loading', value: 'Image_Loading' },
+  { item: 'Object Saving Time', value: 'Object_Saving_Time' },
+  { item: 'Video Watch Time', value: 'Video_Watch_Time' },
+  { item: 'Judgement Time', value: 'Judgement_Time' },
+  { item: 'QA Benchmark', value: 'QA_Benchmark' },
+  { item: 'Annotation', value: 'Annotation' },
+  { item: 'QA', value: 'QA' },
 ];
 const ProjectDirectoryBenchMarkFieldIndex = () => {
   const [anchorEl, setAnchorEl] = useState(null);
@@ -57,7 +56,7 @@ const ProjectDirectoryBenchMarkFieldIndex = () => {
     setBenchmarkItems((prevArray) => [...prevArray, deleteValue]);
     console.log(addBenchmarkItems);
     const filteredArr = addBenchmarkItems.filter((item) => item.value != name);
-    console.log("🚀 ~ handleRemove ~ filteredArr:", filteredArr);
+    console.log('🚀 ~ handleRemove ~ filteredArr:', filteredArr);
     setAddBenchmarkItems(filteredArr);
   };
   return (
@@ -65,23 +64,23 @@ const ProjectDirectoryBenchMarkFieldIndex = () => {
       <Box>
         <Typography
           sx={{
-            mt: "5px",
-            mb: "5px",
-            color: "neutral.N300",
+            mt: '5px',
+            mb: '5px',
+            color: 'neutral.N300',
           }}
-          variant='wpf_h7_medium'
+          variant="wpf_h7_medium"
         >
           Benchmark
         </Typography>
         <Stack
           sx={{
-            border: "1px solid #E6ECF5",
-            padding: "16px",
-            borderRadius: "8px",
+            border: '1px solid #E6ECF5',
+            padding: '16px',
+            borderRadius: '8px',
 
-            background: isLightTheme ? "#FAFCFF" : "#2C2C2C",
+            background: isLightTheme ? '#FAFCFF' : '#2C2C2C',
             // maxHeight: 155,
-            color: isLightTheme ? "#091E42" : "#FFFFFF",
+            color: isLightTheme ? '#091E42' : '#FFFFFF',
             // color: "#FFFFFF",
             // overflowY: "auto",
           }}
@@ -93,13 +92,13 @@ const ProjectDirectoryBenchMarkFieldIndex = () => {
                   <Grid item xs={5.89}>
                     <Box
                       sx={{
-                        width: "100%",
+                        width: '100%',
                         px: 0,
 
                         height: {
-                          lg: "72px",
-                          xl: "82px",
-                          xxl: "85px",
+                          lg: '72px',
+                          xl: '82px',
+                          xxl: '85px',
                         },
                       }}
                     >
@@ -110,42 +109,41 @@ const ProjectDirectoryBenchMarkFieldIndex = () => {
                       />
                     </Box>
                   </Grid>
-                
                 </>
               ))}
           </Grid>
           <Typography
             sx={{
-              fontWeight: "600",
-              mt: "15px",
-              fontSize: "14px",
-              mb: "0px",
+              fontWeight: '600',
+              mt: '15px',
+              fontSize: '14px',
+              mb: '0px',
               //   color: hasChanged ? "#2E58FF" : "#7D89A3",
-              cursor: "pointer",
+              cursor: 'pointer',
               //   pointerEvents: hasChanged ? "auto" : "none",
             }}
-            variant='p'
-            type='button'
+            variant="p"
+            type="button"
             onClick={handleButtonClick}
             // onClick={handleAddOtherDocument}
           >
-            <i className='ri-add-line'></i> Add Another Benchmark
+            <i className="ri-add-line"></i> Add Another Benchmark
           </Typography>
           <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>
             {benchmarkItems.map((filter, index) => (
               <MenuItem
                 sx={{
-                  fontSize: "14px",
-                  "& .MuiInputBase-root": {
+                  fontSize: '14px',
+                  '& .MuiInputBase-root': {
                     // height: "42px",
-                    fontSize: "12px",
-                    fontFamily: "Inter",
-                    "@media(max-width:1439px)": {
+                    fontSize: '12px',
+                    fontFamily: 'Inter',
+                    '@media(max-width:1439px)': {
                       // height: "30px",
-                      fontSize: "10px",
+                      fontSize: '10px',
                     },
-                    "@media(min-width: 1920px)": {
-                      fontSize: "14px",
+                    '@media(min-width: 1920px)': {
+                      fontSize: '14px',
                     },
                   },
                 }}

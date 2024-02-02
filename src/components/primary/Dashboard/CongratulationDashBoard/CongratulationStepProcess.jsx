340cb992fc2924888dcb34d7efd2809dd9c30eb4
Tasfiq97 <tasfiqulazad200@gmail.com>
2024-02-02 14:37:41 +0600

    dashboard

--- src/components/primary/Dashboard/CongratulationDashBoard/CongratulationStepProcess.jsx
+++ src/components/primary/Dashboard/CongratulationDashBoard/CongratulationStepProcess.jsx
@@ -1,14 +1,15 @@
-import {Box, Grid, Paper, Typography} from "@mui/material";
-import {useSelector} from "react-redux";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/scrollbar";
-import {A11y, Navigation} from "swiper/modules";
-import {Swiper, SwiperSlide} from "swiper/react";
-import ProcessCard2, {defaultIndex} from "./ProcessCard2";
-import processList from "./ProcessLIst";
-import SLiderPrevNext from "./SLiderPrevNext";
+import { Box, Grid, Paper, Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import { A11y, Navigation } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import ProcessCard2, { defaultIndex } from './ProcessCard2';
+import processList from './ProcessLIst';
+import SLiderPrevNext from './SLiderPrevNext';
+import ProgressBarForDashboard from '../ProgressBarForDashboard';
 
 const CongratulationStepProcess = () => {
   const { user } = useSelector((state) => state.user);
@@ -16,29 +17,31 @@ const CongratulationStepProcess = () => {
     <>
       <Box
         sx={{
-          width: "100%",
-          borderRadius: "8px",
+          width: '100%',
+          borderRadius: '8px',
         }}
       >
         <Paper
           elevation={0}
           style={{
-            backgroundColor: "neutral.N000",
-            padding: ".5%",
-            borderRadius: "8px",
+            backgroundColor: 'neutral.N000',
+            padding: '.5%',
+            borderRadius: '8px',
           }}
         >
-          <Box sx={{ padding: "1%", display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+          <Box sx={{ padding: '1%', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
             <Box>
               <Grid container>
                 <Typography variant="wpf_h4_semiBold">Quantigo Workforce Process</Typography>
               </Grid>
               <Grid container>
-                <Typography variant="wpf_p3_regular" sx={{ color: "neutral.N300" }}>
+                <Typography variant="wpf_p3_regular" sx={{ color: 'neutral.N300' }}>
                   These are process you have to follow step by step to earn money
                 </Typography>
               </Grid>
             </Box>
+            {/* <Box>hello</Box> */}
+            <ProgressBarForDashboard />
           </Box>
           <Swiper
             modules={[Navigation, A11y]}
@@ -50,7 +53,7 @@ const CongratulationStepProcess = () => {
           >
             <SLiderPrevNext />
 
-            <Box sx={{ padding: "1%" }}>
+            <Box sx={{ padding: '1%' }}>
               <Grid container spacing={3}>
                 {processList.map((item) => (
                   <Grid key={item._id} item xs={12} sm={6} md={3} gap={1}>

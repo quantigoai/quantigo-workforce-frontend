575683f53c71840b2e41237d0dc1902db599cec1
Zayedislam <islamzayed75@gmail.com>
2024-03-20 01:39:44 +0600

    changes courses landing page

--- src/components/primary/Course/hooks/createCourseHook/useCourseMangement.jsx
+++ src/components/primary/Course/hooks/createCourseHook/useCourseMangement.jsx
@@ -1,11 +1,11 @@
-import { yupResolver } from '@hookform/resolvers/yup';
-import React, { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { useDispatch, useSelector } from 'react-redux';
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
-import * as Yup from 'yup';
-import useToaster from '../../../../../customHooks/useToaster';
-import { setActiveChapterIndex, setActiveCourseId } from '../../../../../features/slice/activePathSlice';
+import { yupResolver } from "@hookform/resolvers/yup";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useDispatch, useSelector } from "react-redux";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+import * as Yup from "yup";
+import useToaster from "../../../../../customHooks/useToaster";
+import { setActiveChapterIndex, setActiveCourseId } from "../../../../../features/slice/activePathSlice";
 import {
   createCourse,
   getACourseByID,
@@ -14,7 +14,7 @@ import {
   getAllCoursesNew,
   getArchivedCourses,
   getMyCourses,
-} from '../../../../../features/slice/courseSlice';
+} from "../../../../../features/slice/courseSlice";
 
 const useCourseManagement = () => {
   const { level } = useParams();
@@ -34,7 +34,7 @@ const useCourseManagement = () => {
   const [isDataLoading, setIsDataLoading] = useState(false);
   const [open, setOpen] = useState(false);
   const searchRef = React.useRef(null);
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
   const [isActiveAll, setIsActiveAll] = useState(true);
   const [isActiveEnrolled, setIsActiveEnrolled] = useState(false);
   const [isActiveArchived, setIsActiveArchived] = useState(false);
@@ -45,25 +45,25 @@ const useCourseManagement = () => {
   const [preRequisiteCourses, setPreRequisiteCourses] = React.useState([]);
   const { skills } = useSelector((state) => state.skill);
   const [checkedFeatured, setCheckedFeatured] = useState(false);
-  const [dateTime, setDateTime] = useState('');
-  const [outcomes, setOutcomes] = useState(['']);
+  const [dateTime, setDateTime] = useState("");
+  const [outcomes, setOutcomes] = useState([""]);
   const [skill, setSkill] = React.useState([]);
   const [pagination, setPagination] = useState({
     currentPage: 0,
     pageSize: 10,
   });
   const [buttonClicked, setButtonClicked] = useState(false);
-  const [hub, setHub] = useState(['Dhaka', 'Mymensingh', 'Sirajganj', 'Khulna', 'Chuadanga']);
+  const [hub, setHub] = useState(["Dhaka", "Mymensingh", "Sirajganj", "Khulna", "Chuadanga"]);
   const dispatch = useDispatch();
   const toast = useToaster();
   const { pathname } = useLocation();
   const CourseCreateSchema = Yup.object().shape({
-    name: Yup.string().required('Course name is required'),
-    description: Yup.string().required('Course description is required'),
+    name: Yup.string().required("Course name is required"),
+    description: Yup.string().required("Course description is required"),
 
-    level: Yup.string().required('Course level is required'),
-    category: Yup.string().required('Course category is required'),
-    language: Yup.string().required('Course language is required'),
+    level: Yup.string().required("Course level is required"),
+    category: Yup.string().required("Course category is required"),
+    language: Yup.string().required("Course language is required"),
   });
 
   const handleOpen = () => setOpen(true);
@@ -72,7 +72,7 @@ const useCourseManagement = () => {
 
   const [anchorE2, setAnchorE2] = useState(null);
   const openModal = Boolean(anchorE2);
-  const id = openModal ? 'simple-popover' : undefined;
+  const id = openModal ? "simple-popover" : undefined;
   const handleCloseFilter = () => {
     setAnchorE2(null);
   };
@@ -95,14 +95,14 @@ const useCourseManagement = () => {
         setFilter({});
       });
     } else {
-      if (pathname === '/courses/my-course') {
+      if (pathname === "/courses/my-course") {
         dispatch(getMyCourses({ filter: {}, search, pagination })).then((action) => {
           setCourseCount(action.payload.data.searchedTotal);
           setAllCourses(action.payload.data);
           setIsDataLoading(false);
           setFilter({});
         });
-      } else if (pathname === '/courses/archive-course') {
+      } else if (pathname === "/courses/archive-course") {
         dispatch(getArchivedCourses({ filter: {}, search, pagination })).then((action) => {
           setCourseCount(action.payload.data.searchedTotal);
           setAllCourses(action.payload.data);
@@ -143,13 +143,13 @@ const useCourseManagement = () => {
       });
       // }
     } else {
-      if (pathname === '/courses/my-course') {
+      if (pathname === "/courses/my-course") {
         dispatch(getMyCourses({ filter, search, pagination })).then((action) => {
           setCourseCount(action.payload.data.searchedTotal);
           setAllCourses(action.payload.data);
           // setIsDataLoading(false);
         });
-      } else if (pathname === '/courses/archive-course') {
+      } else if (pathname === "/courses/archive-course") {
         dispatch(getArchivedCourses({ filter, search, pagination })).then((action) => {
           setCourseCount(action.payload.data.searchedTotal);
           setAllCourses(action.payload.data);
@@ -171,16 +171,16 @@ const useCourseManagement = () => {
     reset();
     setPreRequisiteCourses([]);
     setSkill([]);
-    setHub(['Dhaka', 'Mymensingh', 'Sirajganj', 'Khulna', 'Chuadanga']);
+    setHub(["Dhaka", "Mymensingh", "Sirajganj", "Khulna", "Chuadanga"]);
     setCoverImageFile(null);
     setCoverImage(null);
-    setDateTime('');
+    setDateTime("");
     setCheckedFeatured(false);
-    setOutcomes(['']);
+    setOutcomes([""]);
   };
   const methods = useForm({
     resolver: yupResolver(CourseCreateSchema),
-    mode: 'all',
+    mode: "all",
   });
   const handleChangeSkills = (event) => {
     const {
@@ -191,7 +191,7 @@ const useCourseManagement = () => {
       return skills.find((s) => s.name === skill);
     });
 
-    setSkill(typeof selectedSkills === 'string' ? value.split(',') : selectedSkills);
+    setSkill(typeof selectedSkills === "string" ? value.split(",") : selectedSkills);
   };
   const handleChangeHub = (event) => {
     const {
@@ -199,7 +199,7 @@ const useCourseManagement = () => {
     } = event;
     setHub(
       // On autofill we get a stringified value.
-      typeof value === 'string' ? value.split(',') : value
+      typeof value === "string" ? value.split(",") : value
     );
   };
 
@@ -234,7 +234,7 @@ const useCourseManagement = () => {
     });
 
     setPreRequisiteCourses(
-      typeof selectedPreRequisiteCourses === 'string' ? value.split(',') : selectedPreRequisiteCourses
+      typeof selectedPreRequisiteCourses === "string" ? value.split(",") : selectedPreRequisiteCourses
     );
   };
   const { handleSubmit, reset } = methods;
@@ -244,8 +244,8 @@ const useCourseManagement = () => {
   };
 
   const clearSearch = () => {
-    setSearch('');
-    searchRef.current.value = '';
+    setSearch("");
+    searchRef.current.value = "";
   };
   const [isBtnLoading, setIsBtnLoading] = useState(false);
   const onSubmit = (data) => {
@@ -257,26 +257,26 @@ const useCourseManagement = () => {
     });
     const formData = new FormData();
 
-    formData.append('name', data.name);
-    formData.append('category', data.category);
-    formData.append('level', data.level);
-    formData.append('language', data.language);
-    formData.append('description', data.description);
-    formData.append('images', coverImageFile);
-    preRequisiteCoursesColl.length && formData.append('prerequisiteCourses', preRequisiteCoursesColl);
-    hub.length && formData.append('hubs', hub);
-    data.liveSessionLink !== undefined && formData.append('liveSessionLink', data.liveSessionLink);
-    dateTime.$d !== undefined && formData.append('liveSessionStartedAt', dateTime.$d);
-    formData.append('isFeaturedCourse', checkedFeatured);
-    outcomes.length > 1 && formData.append('outComes', outcomes);
-    skillColl.length && formData.append('skills', skillColl);
+    formData.append("name", data.name);
+    formData.append("category", data.category);
+    formData.append("level", data.level);
+    formData.append("language", data.language);
+    formData.append("description", data.description);
+    formData.append("images", coverImageFile);
+    preRequisiteCoursesColl.length && formData.append("prerequisiteCourses", preRequisiteCoursesColl);
+    hub.length && formData.append("hubs", hub);
+    data.liveSessionLink !== undefined && formData.append("liveSessionLink", data.liveSessionLink);
+    dateTime.$d !== undefined && formData.append("liveSessionStartedAt", dateTime.$d);
+    formData.append("isFeaturedCourse", checkedFeatured);
+    outcomes.length > 1 && formData.append("outComes", outcomes);
+    skillColl.length && formData.append("skills", skillColl);
     setIsBtnLoading(true);
     dispatch(createCourse(formData)).then((action) => {
       if (action.error) {
-        toast.trigger(action.error.message, 'error');
+        toast.trigger(action.error.message, "error");
         setIsBtnLoading(false);
       } else {
-        toast.trigger(action.payload.data.message, 'success');
+        toast.trigger(action.payload.data.message, "success");
         if (level) {
           dispatch(getAllCourses({ level, filter, search })).then((action) => {
             setAllCoursesFull(action.payload.data.courses);
@@ -303,18 +303,26 @@ const useCourseManagement = () => {
 
   const handleViewDetailsButton = async (id, courseDirection) => {
     setIsCourseLoading(true);
-    if (courseDirection === 'MyCourse') {
-      navigate(`/course-homepage/${id}`);
-      setIsCourseLoading(false);
-    } else {
-      navigate(`/course-landing/${id}`);
-      setIsCourseLoading(false);
-    }
+    // if (courseDirection === 'MyCourse') {
+    //   navigate(`/course-homepage/${id}`);
+    //   setIsCourseLoading(false);
+    // } else {
+    //   navigate(`/course-landing/${id}`);
+    //   setIsCourseLoading(false);
+    // }
     dispatch(getACourseByID(id))
       .then((res) => {
         dispatch(setActiveCourseId(id));
         dispatch(setActiveChapterIndex(0));
-        dispatch(getAllChapterFromACourse(id)).then((res) => {});
+        dispatch(getAllChapterFromACourse(id)).then((res) => {
+          if (courseDirection === "MyCourse") {
+            navigate(`/course-homepage/${id}`);
+            setIsCourseLoading(false);
+          } else {
+            navigate(`/course-landing/${id}`);
+            setIsCourseLoading(false);
+          }
+        });
       })
       .catch(() => {
         setIsCourseLoading(false);

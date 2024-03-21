d636fdc6762cd19875436e06a7d6564325ed241c
tanzim <tanzimahmed1994@gmail.com>
2024-03-22 01:03:24 +0600

    Refactor code for improved performance and readability

---
+++ src/components/primary/CourseV2/pages/CourseHomepage.jsx
@@ -0,0 +1,27 @@
+/*
+ * File           : CourseHomepage.jsx
+ * Project        : wmpfrontv2
+ * Created Date   : Fr 22 Mar 2024 12:53:51
+ * Description    : <<description>>
+ *
+ * -----------------------------------------------------
+ * Author         : Tanzim Ahmed
+ * Email          : tanzimahmed077@gmail.com
+ * -----------------------------------------------------
+ * Last Modified  : Fri Mar 22 2024
+ * Modified By    : Tanzim Ahmed
+ * -----------------------------------------------------
+ * Copyright (c) 2024 Tanzim Ahmed
+ * -----------------------------------------------------
+ */
+import React from 'react';
+
+const CourseHomepage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CourseHomepage;
---
+++ src/components/primary/CourseV2/pages/CreateQuizPage.jsx
@@ -0,0 +1,16 @@
+/*
+ * File           : CreateQuizPage.jsx
+ * Project        : wmpfrontv2
+ * Created Date   : Fr 22 Mar 2024 12:55:28
+ * Description    : <<description>>
+ *
+ * -----------------------------------------------------
+ * Author         : Tanzim Ahmed
+ * Email          : tanzimahmed077@gmail.com
+ * -----------------------------------------------------
+ * Last Modified  : Fri Mar 22 2024
+ * Modified By    : Tanzim Ahmed
+ * -----------------------------------------------------
+ * Copyright (c) 2024 Tanzim Ahmed
+ * -----------------------------------------------------
+ */

71f716a905e3e55d58cbff046b4090ce1f4a2a06
Tasfiq97 <tasfiqulazad200@gmail.com>
2024-03-21 23:56:32 +0600

    new commit course level see more

--- src/components/primary/CourseV2/pages/AllLeveledCourses.jsx
+++ src/components/primary/CourseV2/pages/AllLeveledCourses.jsx
@@ -14,14 +14,60 @@
  * Copyright (c) 2024 Tanzim Ahmed
  * -----------------------------------------------------
  */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { getAllCourses } from '../../../../features/slice/courseSlice';
+import { Box } from '@mui/material';
+import BasicCard from '../components/CourseCard/BasicCard';
+import CourseHeader from '../shared/courseHeader/CourseHeader';
 
 const AllLeveledCourses = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const dispatch = useDispatch();
+  const [isCourseLoading, setIsCourseLoading] = useState(true);
+  const { level } = useParams();
+  const { isLightTheme } = useSelector((state) => state.theme);
+  useEffect(() => {
+    dispatch(getAllCourses({ level })).then((res) => {
+      setIsCourseLoading(false);
+    });
+  }, [isCourseLoading]);
+  const { courses } = useSelector((state) => state.course);
+
+  return isCourseLoading ? (
+    <div>loading...</div>
+  ) : (
+    <>
+      <CourseHeader />
+      <Box
+        sx={{
+          display: 'grid',
+          p: '25px',
+          gridTemplateColumns: {
+            xxl: 'repeat(4,1fr)',
+            xl: 'repeat(4,1fr)',
+            lg: 'repeat(3,1fr)',
+          },
+          gridGap: '8px',
+          mt: '16px',
+          gap: { xxl: '20px', xl: '15px', lg: '12px' },
+        }}
+      >
+        {courses?.map((course) => (
+          <Box
+            sx={{
+              backgroundColor: isLightTheme ? '#fff' : '#000',
+              width: { xxl: '368px', xl: '278px', lg: '250px' },
+              borderRadius: '10px',
+            }}
+            key={course._id}
+          >
+            <BasicCard isFeaturedShow={true} course={course} />
+          </Box>
+        ))}
+      </Box>
+    </>
+  );
 };
 
 export default AllLeveledCourses;

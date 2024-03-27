d7871fe5ffbc25055b53ddd2c468e77499d273e7
Tasfiq97 <tasfiqulazad200@gmail.com>
2024-03-27 11:30:01 +0600

    new commit

--- src/components/primary/CourseV2/pages/AllLeveledCourses.jsx
+++ src/components/primary/CourseV2/pages/AllLeveledCourses.jsx
@@ -53,16 +53,15 @@ const AllLeveledCourses = () => {
       <LoadingComponent />
     </>
   ) : (
-    <Box sx={{ px: '25px', height: ' 100%', padding: '0', margin: '0' }}>
-      <Box sx={{ height: '9%' }}>
-        <CourseHeader level={level} />
-      </Box>
+    <Box sx={{ px: '25px', height: ' 100%' }}>
+      <CourseHeader level={level} />
+
       <Box
         sx={{
-          height: '91%',
+          height: '90%',
           overflow: 'auto',
           display: 'grid',
-          p: '25px',
+          // p: '25px',
           gridTemplateColumns: {
             xxl: 'repeat(4,1fr)',
             xl: 'repeat(4,1fr)',
